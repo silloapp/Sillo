0d228e146a4b2ba62f1eb31f5760153b30ff7748
Eashan Mathur <eashanmathur@gmail.com>
2021-01-02 16:24:06 -0800

    Skeleton

---
+++ Sillo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sillo
+//
+//  Created by Eashan Mathur on 1/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
