aed54c000e4c8ed8a92bbbb3997ecde8640cf021
Eashan Mathur <eashanmathur@gmail.com>
2021-01-13 18:37:38 -0800

    Onboarding update (#8)

--- Sillo/Assets/Colors/Colors.swift
+++ Sillo/Assets/Colors/Colors.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct Color {
     
-    static var getStarted: UIColor {
+    static var gray: UIColor {
         return UIColor(displayP3Red: 224/255, green: 224/255, blue: 224/255, alpha: 1.0)
     }
     
@@ -18,8 +18,16 @@ struct Color {
         return UIColor(displayP3Red: 64/255, green: 102/255, blue: 234/255, alpha: 1.0)
     }
     
+    static var buttonClickableUnselected: UIColor {
+        return UIColor(displayP3Red: 64/255, green: 102/255, blue: 234/255, alpha: 0.5)
+    }
+    
     static var textSemiBlack: UIColor {
         return UIColor(displayP3Red: 51/255, green: 51/255, blue: 51/255, alpha: 1.0)
+    }
+    
+    static var progressBlue: UIColor {
+        return UIColor(displayP3Red: 62/255, green: 139/255, blue: 255/255, alpha: 1.0)
     }
     
     static var textFieldBackground = UIColor(red: CGFloat(249/255), green: CGFloat(249/255), blue: CGFloat(249/255), alpha: CGFloat(0.05))
