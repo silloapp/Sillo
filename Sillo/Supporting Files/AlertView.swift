499ca54250ed190cb8a3c81aba371fdc1488d31b
Eashan Mathur <eashanmathur@gmail.com>
2021-03-24 13:50:30 -0400

    Finished popup and adaptive image and image constraints (#56)

--- Sillo/Supporting Files/AlertView.swift
+++ Sillo/Supporting Files/AlertView.swift
@@ -40,16 +40,38 @@ class AlertView: UIViewController {
         return btn
     }()
     
+    var cancelBtn: UIButton = {
+        var btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
+    var alertImageView: UIImageView = {
+        var img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.contentMode = .scaleAspectFit
+        return img
+    }()
+    
+    
+    var withCancel: Bool
+    var withOneButton: Bool
     var btn1Action: () -> Void
     var btn2Action: () -> Void
     
-    init(headingText: String, messageText: String, action1Label btn1String: String, action1Color btn1Color: UIColor, action1Completion: @escaping () -> Void, action2Label btn2String: String, action2Color btn2Color: UIColor, action2Completion: @escaping () -> Void) {
+    
+    init(headingText: String, messageText: String, action1Label btn1String: String, action1Color btn1Color: UIColor, action1Completion: @escaping () -> Void, action2Label btn2String: String, action2Color btn2Color: UIColor, action2Completion: @escaping () -> Void, withCancelBtn: Bool, image: UIImage?, withOnlyOneAction: Bool) {
         
         btn1Action = action1Completion
         btn2Action = action2Completion
+        withCancel = withCancelBtn
+        withOneButton = withOnlyOneAction
         
         super.init(nibName: nil, bundle: nil)
         
+        if let img = image {
+            alertImageView.image = img
+        }
         
         heading.text = headingText
         message.text = messageText
@@ -63,12 +85,12 @@ class AlertView: UIViewController {
         btn1.addTarget(self, action: #selector(btn1Function), for: .touchUpInside)
         btn2.addTarget(self, action: #selector(btn2Function), for: .touchUpInside)
         
+        
         btn1.titleLabel?.adjustsFontSizeToFitWidth = true
         btn2.titleLabel?.adjustsFontSizeToFitWidth = true
         
         btn1.layer.shadowOpacity = 0
         btn2.layer.shadowOpacity = 0
-        
         
         
         if !UIAccessibility.isReduceTransparencyEnabled {
@@ -102,6 +124,10 @@ class AlertView: UIViewController {
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "PopupDidAppear"), object: nil)
     }
     
+    @objc func dismissPop() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -111,6 +137,7 @@ class AlertView: UIViewController {
         view.backgroundColor = .white
         setupView()
     }
+    
     
     func setupView(){
         
@@ -124,9 +151,15 @@ class AlertView: UIViewController {
         Vstack.spacing = 10
         Vstack.distribution = .fillProportionally
         
+        if alertImageView.image != nil {
+            Vstack.addArrangedSubview(alertImageView)
+        }
+        
         Vstack.addArrangedSubview(heading)
         Vstack.addArrangedSubview(message)
         Vstack.translatesAutoresizingMaskIntoConstraints = false
+        
+
         
         let Hstack = UIStackView()
         Hstack.axis = .horizontal
@@ -134,7 +167,9 @@ class AlertView: UIViewController {
         Hstack.spacing = 15
         
         Hstack.addArrangedSubview(btn1)
-        Hstack.addArrangedSubview(btn2)
+        if !withOneButton {
+            Hstack.addArrangedSubview(btn2)
+        }
         Hstack.translatesAutoresizingMaskIntoConstraints = false
         
         backgroundView.addSubview(Vstack)
@@ -144,20 +179,37 @@ class AlertView: UIViewController {
         
         backgroundView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         backgroundView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        backgroundView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 304/375).isActive = true
         backgroundView.layer.cornerRadius = 12
         
         Vstack.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor).isActive = true
         Vstack.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 15).isActive = true
-        Vstack.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 15).isActive = true
-        Vstack.widthAnchor.constraint(equalToConstant: 220).isActive = true
+        Vstack.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 28).isActive = true
+        Vstack.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -28).isActive = true
         
         Hstack.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor).isActive = true
         Hstack.topAnchor.constraint(equalTo: Vstack.bottomAnchor, constant: 20).isActive = true
-        Hstack.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        if !withOneButton {
+            Hstack.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        } else {
+            Hstack.widthAnchor.constraint(equalToConstant: 130).isActive = true
+        }
         Hstack.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: -14).isActive = true
         
         btn1.heightAnchor.constraint(equalToConstant: 30).isActive = true
         btn2.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+            
+        if withCancel {
+            cancelBtn.setImage(UIImage(named: "closeButton"), for: .normal)
+            cancelBtn.addTarget(self, action: #selector(self.dismissPop), for: .touchUpInside)
+            backgroundView.addSubview(cancelBtn)
+            backgroundView.bringSubviewToFront(cancelBtn)
+            cancelBtn.setTitleColor(.black, for: .normal)
+            
+            cancelBtn.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 15).isActive = true
+            cancelBtn.leftAnchor.constraint(equalTo: backgroundView.leftAnchor, constant: 15).isActive = true
+        }
         
     }
     
