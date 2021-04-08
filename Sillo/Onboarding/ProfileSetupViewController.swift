459d1e55933e9cbed9787a27dde1f296353617c8
Eashan Mathur <eashanmathur@gmail.com>
2021-04-08 00:55:25 -0700

    - Fixed the profile button not showing up - Added new images for the tabs in the the Team Tab - Fixed the scrolling issue in the "add a space" Half VC where the add button scrolled off-screen when scrolling in the TableView

--- Sillo/Onboarding/ProfileSetupViewController.swift
+++ Sillo/Onboarding/ProfileSetupViewController.swift
@@ -417,7 +417,7 @@ class ProfileSetupViewController: UIViewController{
         
         //MARK: next button container
         view.addSubview(saveChangesContainer)
-        saveChangesContainer.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        saveChangesContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         saveChangesContainer.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         saveChangesContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 90/812).isActive = true
         saveChangesContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -425,7 +425,7 @@ class ProfileSetupViewController: UIViewController{
         //MARK: next button
         saveChangesContainer.addSubview(nextButton)
         nextButton.centerXAnchor.constraint(equalTo: saveChangesContainer.centerXAnchor).isActive = true
-        nextButton.centerYAnchor.constraint(equalTo: saveChangesContainer.centerYAnchor, constant: -10).isActive = true
+        nextButton.centerYAnchor.constraint(equalTo: saveChangesContainer.centerYAnchor, constant: -5).isActive = true
         nextButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 50/812).isActive = true
         nextButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
         
@@ -520,6 +520,7 @@ class ProfileSetupViewController: UIViewController{
         print("using pronouns \(self.pronouns)")
         pronounsTextField.placeholder = self.pronouns
         pronounsTextField.text = self.pronouns
+        pronounsTextField.font = Font.regular(17)
         scrollView.addSubview(pronounsTextField)
         pronounsTextField.attributedPlaceholder =
             NSAttributedString(string: self.pronouns, attributes: [NSAttributedString.Key.foregroundColor : Color.matte])
