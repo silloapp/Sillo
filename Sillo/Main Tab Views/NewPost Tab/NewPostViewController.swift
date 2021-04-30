fbb9d154eb89363a676a4c768cdac5a091f91dd3
angelicapan <angelica.pan@berkeley.edu>
2021-04-30 10:51:45 +0700

    fixed capitalization issue (#119)

--- Sillo/Main Tab Views/NewPost Tab/NewPostViewController.swift
+++ Sillo/Main Tab Views/NewPost Tab/NewPostViewController.swift
@@ -160,52 +160,42 @@ class NewPostViewController: UIViewController, UITextViewDelegate {
     }
     
     
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-
-        // Combine the textView text and the replacement text to
-        // create the updated text string
-        let currentText:String = textView.text
-        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
-
-        // If updated text view will be empty, add the placeholder
-        // and set the cursor to the beginning of the text view
-        if updatedText.isEmpty {
-            
-            textView.text = PLACEHOLDER_TEXT
-            textView.textColor = UIColor.lightGray
-            
-            textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
-            newPostButton.backgroundColor = Color.buttonClickableUnselected
-
-        }
-
-        // Else if the text view's placeholder is showing and the
-        // length of the replacement string is greater than 0, set
-        // the text color to black then set its text to the
-        // replacement string
-         else if textView.textColor == UIColor.lightGray && !text.isEmpty {
-            textView.textColor = UIColor.black
-            textView.text = text
-            newPostButton.backgroundColor = Color.buttonClickable
-
-        }
-
-        // For every other case, the text should change with the usual
-        // behavior...
-        else {
-            return true
-        }
-
-        // ...otherwise return false since the updates have already
-        // been made
-        return false
+    
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) {
+    if textView.textColor == .lightGray {
+//    textView.text = ""
+    textView.textColor = .black
+    }
     }
     
-    func textViewDidChangeSelection(_ textView: UITextView) {
-        if self.view.window != nil {
-            if textView.textColor == UIColor.lightGray {
-                textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
-            }
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if textView.text == PLACEHOLDER_TEXT {
+            textView.text = ""
+        }
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+    if textView.text.isEmpty {
+    textView.text = PLACEHOLDER_TEXT
+    textView.textColor = UIColor.lightGray
+    }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+    let currText = textView.text
+        if currText == PLACEHOLDER_TEXT {
+        textView.textColor = .lightGray
+        } else {
+        textView.textColor = .black
+        }
+        
+        if currText == "" {
+            textView.text = PLACEHOLDER_TEXT
+            textView.textColor = .lightGray
+            let newPosition = textView.beginningOfDocument
+            textView.selectedTextRange = textView.textRange(from: newPosition, to: newPosition)
         }
     }
     
