93b95b43a189c83c0b34e54f5fa5abe8aec45944
William Loo <williamloo@berkeley.edu>
2021-06-06 20:57:01 -0700

    horizontal segue, remove haptics (#136)

--- Sillo/Supporting Files/CustomTabBarController.swift
+++ Sillo/Supporting Files/CustomTabBarController.swift
@@ -26,12 +26,6 @@ class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
     
     //MARK: UITabbar Delegate
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        
-        //haptics upon switching
-        let generator = UIImpactFeedbackGenerator(style: .light)
-        generator.prepare()
-        generator.impactOccurred()
-        
         if viewController.isKind(of: NewPostViewController.self) {
 
             let vc =  NewPostViewController()
@@ -42,4 +36,66 @@ class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
         return true
     }
     
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SlideTransition(viewControllers: tabBarController.viewControllers)
+    }
+    
+}
+
+//https://stackoverflow.com/questions/44346280/how-to-animate-tab-bar-tab-switch-with-a-crossdissolve-slide-transition
+//keywords: hor segue, horizontal slide, 
+class SlideTransition: NSObject, UIViewControllerAnimatedTransitioning {
+
+    let viewControllers: [UIViewController]?
+    let transitionDuration: Double = 0.3
+
+    init(viewControllers: [UIViewController]?) {
+        self.viewControllers = viewControllers
+    }
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return TimeInterval(transitionDuration)
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+
+        guard
+            let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from),
+            let fromView = fromVC.view,
+            let fromIndex = getIndex(forViewController: fromVC),
+            let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to),
+            let toView = toVC.view,
+            let toIndex = getIndex(forViewController: toVC)
+            else {
+                transitionContext.completeTransition(false)
+                return
+        }
+
+        let frame = transitionContext.initialFrame(for: fromVC)
+        var fromFrameEnd = frame
+        var toFrameStart = frame
+        fromFrameEnd.origin.x = toIndex > fromIndex ? frame.origin.x - frame.width : frame.origin.x + frame.width
+        toFrameStart.origin.x = toIndex > fromIndex ? frame.origin.x + frame.width : frame.origin.x - frame.width
+        toView.frame = toFrameStart
+
+        DispatchQueue.main.async {
+            transitionContext.containerView.addSubview(toView)
+            UIView.animate(withDuration: self.transitionDuration, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.1, options: [.curveEaseInOut], animations: {
+                fromView.frame = fromFrameEnd
+                toView.frame = frame
+            }, completion:  {success in
+                fromView.removeFromSuperview()
+                transitionContext.completeTransition(success)
+            })
+        }
+    }
+
+    //get index to determine direction
+    func getIndex(forViewController vc: UIViewController) -> Int? {
+        guard let vcs = self.viewControllers else { return nil }
+        for (index, thisVC) in vcs.enumerated() {
+            if thisVC == vc { return index }
+        }
+        return nil
+    }
 }
